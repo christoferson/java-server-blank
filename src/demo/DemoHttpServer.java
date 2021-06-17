664c51f47d45da5319959a24074749032786fa23
christoferson <85178973+christoferson@users.noreply.github.com>
2021-06-17 23:01:34 +0900

    update

--- src/demo/DemoHttpServer.java
+++ src/demo/DemoHttpServer.java
@@ -12,17 +12,44 @@ public class DemoHttpServer {
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
-        HttpContext context = server.createContext("/");
-        context.setHandler(DemoHttpServer::handleRequest);
+        {
+        	HttpContext context = server.createContext("/");
+        	context.setHandler(DemoHttpServer::handleRequest);
+        }
+        {
+        	HttpContext context = server.createContext("/foo");
+        	context.setHandler(DemoHttpServer::handleRequestFoo);
+        }
+        {
+        	HttpContext context = server.createContext("/bar");
+        	context.setHandler(DemoHttpServer::handleRequestBar);
+        }
+        System.out.println("Starting Server...");
         server.start();
     }
 
     private static void handleRequest(HttpExchange exchange) throws IOException {
-        String response = "Hi there!";
-        exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
+        String response = "Hi there! " + System.currentTimeMillis();
+        exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
     }
     
+    private static void handleRequestFoo(HttpExchange exchange) throws IOException {
+        String response = "Foo! " + System.currentTimeMillis();
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }    
+
+    private static void handleRequestBar(HttpExchange exchange) throws IOException {
+        String response = "Bar! " + System.currentTimeMillis();
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
 }
